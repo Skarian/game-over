166573a6ef30b96179f55212461fda716f39ef1e
Neil Skaria <neil.skaria@gmail.com>
2025-01-09 21:43:20 -0600

    Added enter sound and simplified

--- src/Focus.tsx
+++ src/Focus.tsx
@@ -1,8 +1,26 @@
 import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
 import { useEffect } from "react";
+import navigationSound from "/sounds/deck_ui_navigation.mp3"
+import enterSound from "/sounds/deck_ui_into_game_detail.mp3"
+import useSound from "use-sound";
 
 const Focus: React.FC<{ action: () => void, autoFocus: boolean, children: React.ReactNode, focusedStyle: string, unFocusedStyle: string, className: string }> = ({ action, autoFocus, children, focusedStyle, unFocusedStyle, className }) => {
-  const { ref, focusSelf, focused } = useFocusable({ onEnterRelease: action });
+  const [playNavigation] = useSound(navigationSound);
+  const [playEnter] = useSound(enterSound);
+  const { ref, focusSelf, focused } = useFocusable({
+    onEnterRelease: () => {
+      action()
+      playEnter()
+    },
+    // onArrowPress: (direction, props, details) => {
+    //   playNavigation()
+    //   return true
+    // },
+    onFocus: () => {
+      playNavigation()
+    }
+  });
+
 
   useEffect(() => {
     if (autoFocus) {
